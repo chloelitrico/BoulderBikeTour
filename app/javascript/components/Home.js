52ca2906b051cfe79f6b940850eabc4762df18fa
Chloe Litrico <chloelitrico@chloes-macbook-pro.home>
2019-05-28 17:39:17 +0200

    Countdown timer

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Counter from './Counter'
+import "./Home.css"
 
 class Home extends React.Component {
 	render() {
     	return (
-			<div>
+			<div class="page_body">
 				Home page!
+				<Counter />
 			</div>
 		);
 	}
